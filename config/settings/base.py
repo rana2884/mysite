7903e804b8784ae3868c8f03ee160e1a9248a086
Gaurav S Rana <grana@urjakart.com>
2016-10-01 23:45:52 +0530

    allowed host

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,7 +3,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.gsrana.com',
+]
 
 # Application definition
 INSTALLED_APPS = [
