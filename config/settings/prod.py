b9eb501dff9da94191eb20777544d77600a15c1f
Gaurav S Rana <grana@urjakart.com>
2016-10-02 20:48:30 +0530

    home page improvement

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,13 @@
 from .base import *
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+STATIC_URL = '/static/'
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'key'
 
