bb8e68f6a4e104e87fa516f8fc8ecee0b3c7aefc
Gaurav S Rana <grana@urjakart.com>
2016-10-05 23:31:40 +0530

    login improvement

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^blog/$', views.blog),
     url(r'^blog/(?P<pk>\d+)/$', views.post),
     url(r'^contact/$', views.contact),
+    url(r'^logout/$', views.logout_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,70 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import Post
 from .forms import ContactForm
 
 
 def index(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return render(request, 'blog/account.html')
-            else:
-                return render(request, 'blog/home.html', {'login_msg': "Your account is disabled"})
-        else:
-                return render(request, 'blog/home.html', {'login_msg': "Invalid login details"})
+    if request.method == 'POST' and request.POST['login'] == 'Login':
+        login_msg = login_user(request)
+        return render(request, 'blog/home.html', {'login_msg': login_msg})
     else:
         return render(request, 'blog/home.html', {})
 
 
 def blog(request):
-    return render(request, 'blog/blog.html', {'mylist': Post.objects.all().order_by("-date")})
+    if request.method == 'POST' and request.POST['login'] == 'Login':
+        login_msg = login_user(request)
+        return render(request, 'blog/blog.html', {'login_msg': login_msg, 'mylist': Post.objects.all().order_by("-date")})
+    else:
+        return render(request, 'blog/blog.html', {'mylist': Post.objects.all().order_by("-date")})
 
 
 def post(request, pk):
-    return render(request, 'blog/post.html', {'mypost': Post.objects.get(id=pk),'mylist': Post.objects.all().order_by("-date")})
+    if request.method == 'POST' and request.POST['login'] == 'Login':
+        login_msg = login_user(request)
+        return render(request, 'blog/post.html', {'login_msg': login_msg, 'mypost': Post.objects.get(id=pk), 'mylist': Post.objects.all().order_by("-date")})
+    else:
+        return render(request, 'blog/post.html', {'mypost': Post.objects.get(id=pk),'mylist': Post.objects.all().order_by("-date")})
 
 
 def contact(request):
     if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
-            form = ContactForm()
-            return render(request, 'blog/contact.html', {'form': form, 'thank': True})
+        form = ContactForm()
+        try:
+            x = request.POST['login']
+        except KeyError:
+            x = None
+        if x == 'Login':
+            login_msg = login_user(request)
+            return render(request, 'blog/contact.html', {'login_msg': login_msg, 'form': form})
+        else:
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                form.save(commit=True)
+                form = ContactForm()
+                return render(request, 'blog/contact.html', {'form': form, 'thank': True})
     else:
         form = ContactForm()
     return render(request, 'blog/contact.html', {'form': form})
+
+
+def logout_view(request):
+        logout(request)
+        return redirect(index)
+
+
+# Helper function
+def login_user(request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                msg = ""
+            else:
+                msg = "Your account is disabled"
+        else:
+            msg = "Invalid login details"
+        return msg
