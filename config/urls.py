aefe608e31bccc06dd308faf721ff49167314004
Gaurav S Rana <grana@urjakart.com>
2016-10-08 22:48:46 +0530

    backend url redirect

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from blog.decorators import login_required_message_and_redirect
+
+admin.autodiscover()
+admin.site.login = login_required_message_and_redirect(admin.site.login, message='Please login with staff credentials')
 
 
 urlpatterns = [
