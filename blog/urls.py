6f1493ba9556561827463bacbffea4872b527110
Gaurav S Rana <grana@urjakart.com>
2016-10-08 21:27:06 +0530

    namespace url

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^blog/$', views.blog_view),
-    url(r'^blog/(?P<pk>\d+)/$', views.post_view),
-    url(r'^contact/$', views.contact_view),
-    url(r'^logout/$', views.logout_view),
+    url(r'^$', views.index, name='blog-index'),
+    url(r'^blog/$', views.blog_view, name='blog-list'),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_view, name='blog-post'),
+    url(r'^contact/$', views.contact_view, name='contact'),
+    url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^captcha/', include('captcha.urls')),
 ]
