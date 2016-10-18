264d2b5996b4b55c4b920b99d66dfbd10bc08bb5
Gaurav S Rana <grana@urjakart.com>
2016-10-18 16:48:41 +0530

    minor import comment changes

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from captcha.fields import CaptchaField
-from django.contrib.auth.models import User
 from .models import Contact
 
 
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from core.decorators import login_required_message_and_redirect
-
-#admin.autodiscover()
-#admin.site.login = login_required_message_and_redirect(admin.site.login, message='Please login with staff credentials')
 
 
 urlpatterns = [
