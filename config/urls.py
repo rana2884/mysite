f02b6d310ff1501ce8321f2386329d85b37e2ad8
Gaurav S Rana <grana@urjakart.com>
2016-10-18 18:32:58 +0530

    login auth

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('blog.urls')),
-    url(r'^dashboard/', include('dashboard.urls'))
+    url(r'^dashboard/', include('dashboard.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 ]
